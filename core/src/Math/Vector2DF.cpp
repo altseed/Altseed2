51178163acbe00f8fbd7330183ab7bfe5b73b6ad
steLLa-m@gica <s1711399@coins.tsukuba.ac.jp>
2019-09-26 23:20:17 +0900

    update: Vector2DF,2DI

---
+++ core/src/Math/Vector2DF.cpp
@@ -0,0 +1,77 @@
+#include "Vector2DF.h"
+#include "Vector2DI.h"
+
+namespace altseed {
+Vector2DF::Vector2DF() : X(0.0f), Y(0.0f) {}
+
+Vector2DF::Vector2DF(float x, float y) : X(x), Y(y) {}
+
+bool Vector2DF::operator==(const Vector2DF& right) { return X == right.X && Y == right.Y; }
+
+bool Vector2DF::operator!=(const Vector2DF& right) { return X != right.X || Y != right.Y; }
+
+bool Vector2DF::operator>(const Vector2DF& o) const {
+    if (X != o.X) return X > o.X;
+    if (Y != o.Y) return Y > o.Y;
+    return false;
+}
+
+bool Vector2DF::operator<(const Vector2DF& o) const {
+    if (X != o.X) return X < o.X;
+    if (Y != o.Y) return Y < o.Y;
+    return false;
+}
+
+Vector2DF Vector2DF::operator-() { return Vector2DF(-X, -Y); }
+
+Vector2DF Vector2DF::operator+(const Vector2DF& right) const { return Vector2DF(X + right.X, Y + right.Y); }
+
+Vector2DF Vector2DF::operator-(const Vector2DF& right) const { return Vector2DF(X - right.X, Y - right.Y); }
+
+Vector2DF Vector2DF::operator*(const Vector2DF& right) const { return Vector2DF(X * right.X, Y * right.Y); }
+
+Vector2DF Vector2DF::operator/(const Vector2DF& right) const { return Vector2DF(X / right.X, Y / right.Y); }
+
+Vector2DF Vector2DF::operator*(float right) const { return Vector2DF(X * right, Y * right); }
+
+Vector2DF Vector2DF::operator/(float right) const { return Vector2DF(X / right, Y / right); }
+
+Vector2DF& Vector2DF::operator+=(const Vector2DF& right) {
+    X += right.X;
+    Y += right.Y;
+    return *this;
+}
+
+Vector2DF& Vector2DF::operator-=(const Vector2DF& right) {
+    X -= right.X;
+    Y -= right.Y;
+    return *this;
+}
+
+Vector2DF& Vector2DF::operator*=(const Vector2DF& right) {
+    X *= right.X;
+    Y *= right.Y;
+    return *this;
+}
+
+Vector2DF& Vector2DF::operator/=(const Vector2DF& right) {
+    X /= right.X;
+    Y /= right.Y;
+    return *this;
+}
+
+Vector2DF& Vector2DF::operator*=(float right) {
+    X *= right;
+    Y *= right;
+    return *this;
+}
+
+Vector2DF& Vector2DF::operator/=(float right) {
+    X /= right;
+    Y /= right;
+    return *this;
+}
+
+Vector2DI Vector2DF::To2DI() const { return Vector2DI((int32_t)X, (int32_t)Y); }
+
+}  // namespace altseed
---
+++ core/src/Math/Vector2DI.cpp
@@ -0,0 +1,67 @@
+#include "Vector2DF.h"
+#include "Vector2DI.h"
+
+namespace altseed {
+Vector2DI::Vector2DI() : X(0), Y(0) {}
+
+Vector2DI::Vector2DI(int32_t x, int32_t y) : X(x), Y(y) {}
+
+bool Vector2DI::operator==(const Vector2DI& right) { return X == right.X && Y == right.Y; }
+
+bool Vector2DI::operator!=(const Vector2DI& right) { return X != right.X || Y != right.Y; }
+
+Vector2DI Vector2DI::operator-() { return Vector2DI(-X, -Y); }
+
+Vector2DI Vector2DI::operator+(const Vector2DI& right) { return Vector2DI(X + right.X, Y + right.Y); }
+
+Vector2DI Vector2DI::operator-(const Vector2DI& right) { return Vector2DI(X - right.X, Y - right.Y); }
+
+Vector2DI Vector2DI::operator*(const Vector2DI& right) { return Vector2DI(X * right.X, Y * right.Y); }
+
+Vector2DI Vector2DI::operator/(const Vector2DI& right) { return Vector2DI(X / right.X, Y / right.Y); }
+
+Vector2DI Vector2DI::operator*(int32_t right) { return Vector2DI(X * right, Y * right); }
+
+Vector2DI Vector2DI::operator/(int32_t right) { return Vector2DI(X / right, Y / right); }
+
+Vector2DI& Vector2DI::operator+=(const Vector2DI& right) {
+    X += right.X;
+    Y += right.Y;
+    return *this;
+}
+
+Vector2DI& Vector2DI::operator-=(const Vector2DI& right) {
+    X -= right.X;
+    Y -= right.Y;
+    return *this;
+}
+
+Vector2DI& Vector2DI::operator*=(const Vector2DI& right) {
+    X *= right.X;
+    Y *= right.Y;
+    return *this;
+}
+
+Vector2DI& Vector2DI::operator/=(const Vector2DI& right) {
+    X /= right.X;
+    Y /= right.Y;
+    return *this;
+}
+
+Vector2DI& Vector2DI::operator*=(int32_t right) {
+    X *= right;
+    Y *= right;
+    return *this;
+}
+
+Vector2DI& Vector2DI::operator/=(int32_t right) {
+    X /= right;
+    Y /= right;
+    return *this;
+}
+
+Vector2DF Vector2DI::To2DF() const { return Vector2DF(X, Y); }
+
+Vector2DI Vector2DI::DivideByScalar(const Vector2DI& v1, float v2) { return Vector2DI(v1.X / v2, v1.Y / v2); }
+
+}  // namespace altseed
