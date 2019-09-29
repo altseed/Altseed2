f532cde14e14d3e1dcf76efd6ba43601fe7f4db9
steLLa-m@gica <s1711399@coins.tsukuba.ac.jp>
2019-09-29 19:22:51 +0900

    fix: Vector4DF.cpp

--- core/src/Math/Vector4DF.cpp
+++ core/src/Math/Vector4DF.cpp
@@ -1,9 +1,7 @@
-#pragma once
-
 //----------------------------------------------------------------------------------
-// Include
+//
 //----------------------------------------------------------------------------------
-#include "MathTemplate.h"
+#include "Vector4DF.h"
 
 //----------------------------------------------------------------------------------
 //
@@ -12,126 +10,73 @@ namespace altseed {
 //----------------------------------------------------------------------------------
 //
 //----------------------------------------------------------------------------------
-/**
-@brief	4次元ベクトル
-*/
-struct Vector4DF {
-public:
-    /**
-    @brief	X成分
-    */
-    float X;
+Vector4DF::Vector4DF() : X(0), Y(0), Z(0), W(0) {}
 
-    /**
-    @brief	Y成分
-    */
-    float Y;
+Vector4DF::Vector4DF(float x, float y, float z, float w) : X(x), Y(y), Z(z), W(w) {}
 
-    /**
-    @brief	Z成分
-    */
-    float Z;
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+bool Vector4DF::operator==(const Vector4DF& o) { return X == o.X && Y == o.Y && Z == o.Z && W == o.W; }
 
-    /**
-    @brief	W成分
-    */
-    float W;
-
-    /**
-    @brief	コンストラクタ
-    */
-    Vector4DF();
-
-    /**
-    @brief	コンストラクタ
-    @param	x	X成分
-    @param	y	Y成分
-    @param	z	Z成分
-    @param	w	W成分
-    */
-    Vector4DF(float x, float y, float z, float w);
-
-    /**
-    @brief	このベクトルの長さを取得する。
-    */
-    float GetLength() const { return sqrt(GetSquaredLength()); }
-
-    /**
-    @brief	このベクトルの長さの二乗を取得する。
-    */
-    float GetSquaredLength() const { return X * X + Y * Y + Z * Z + W * W; }
-
-    /**
-    @brief	このベクトルの長さを設定する。
-    */
-    void SetLength(float value) {
-        float length = GetLength();
-        (*this) *= (value / length);
-    }
-
-    /**
-    @brief	このベクトルの単位ベクトルを取得する。
-    */
-    Vector4DF GetNormal() {
-        float length = GetLength();
-        return Vector4DF(X / length, Y / length, Z / length, W / length);
-    }
-
-    /**
-    @brief	このベクトルの単位ベクトル化する。
-    */
-    void Normalize() {
-        float length = GetLength();
-        (*this) /= length;
-    }
-
-    bool operator==(const Vector4DF& o);
-    bool operator!=(const Vector4DF& o);
-
-    Vector4DF operator-();
-
-    Vector4DF operator+(const Vector4DF& o) const;
-
-    Vector4DF operator-(const Vector4DF& o) const;
-
-    Vector4DF operator*(const Vector4DF& o) const;
-
-    Vector4DF operator/(const Vector4DF& o) const;
-
-    Vector4DF operator*(const float& o) const;
-
-    Vector4DF operator/(const float& o) const;
-
-    Vector4DF& operator+=(const Vector4DF& o);
-
-    Vector4DF& operator-=(const Vector4DF& o);
-
-    Vector4DF& operator*=(const Vector4DF& o);
-
-    Vector4DF& operator/=(const Vector4DF& o);
-
-    Vector4DF& operator*=(const float& o);
-
-    Vector4DF& operator/=(const float& o);
-
-    /**
-    @brief	内積を取得する。
-    @param	v1	v1ベクトル
-    @param	v2	v2ベクトル
-    @return 内積v1・v2
-    */
-    static float Dot(const Vector4DF& v1, const Vector4DF& v2);
-
-    /**
-    @brief	2点間の距離を取得する。
-    @param	v1	v1ベクトル
-    @param	v2	v2ベクトル
-    @return v1とv2の距離
-    */
-    static float Distance(const Vector4DF& v1, const Vector4DF& v2);
-};
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+bool Vector4DF::operator!=(const Vector4DF& o) { return !(X == o.X && Y == o.Y && Z == o.Z && W == o.W); }
 
 //----------------------------------------------------------------------------------
 //
 //----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator-() { return Vector4DF(-X, -Y, -Z, -W); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator+(const Vector4DF& o) const { return Vector4DF(X + o.X, Y + o.Y, Z + o.Z, W + o.W); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator-(const Vector4DF& o) const { return Vector4DF(X - o.X, Y - o.Y, Z - o.Z, W - o.W); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator*(const Vector4DF& o) const { return Vector4DF(X * o.X, Y * o.Y, Z * o.Z, W * o.W); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator/(const Vector4DF& o) const { return Vector4DF(X / o.X, Y / o.Y, Z / o.Z, W / o.W); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator*(const float& o) const { return Vector4DF(X * o, Y * o, Z * o, W * o); }
+
+//-----------------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------------
+Vector4DF Vector4DF::operator/(const float& o) const { return Vector4DF(X / o, Y / o, Z * o, W * o); }
+
+//----------------------------------------------------------------------------------
+//
+//----------------------------------------------------------------------------------
+float Vector4DF::Dot(const Vector4DF& v1, const Vector4DF& v2) { return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z + v1.W * v2.W; }
+
+//----------------------------------------------------------------------------------
+//
+//----------------------------------------------------------------------------------
+float Vector4DF::Distance(const Vector4DF& v1, const Vector4DF& v2) {
+    float dx = v1.X - v2.X;
+    float dy = v1.Y - v2.Y;
+    float dz = v1.Z - v2.Z;
+    float dw = v1.W - v2.W;
+    return sqrt(dx * dx + dy * dy + dz * dz + dw * dw);
+}
+
+//----------------------------------------------------------------------------------
+//
+//----------------------------------------------------------------------------------
+
 }  // namespace altseed
