d5ef08e542599f56ef32c23c641c7e6df5e68f4b
GON <hutabasuzukiryu@live.jp>
2019-10-02 19:20:14 +0900

    fix: merge miss

--- core/src/IO/BaseFileReader.cpp
+++ core/src/IO/BaseFileReader.cpp
@@ -3,14 +3,26 @@
 #include "../Common/StringHelper.h"
 
 namespace altseed {
+
 BaseFileReader::BaseFileReader(const std::u16string& path) : BaseObject(), m_path(path), m_length(-1), m_position(0) {
-    m_file.open(utf16_to_utf8(path), std::basic_ios<char>::in | std::basic_ios<char>::binary);
+    if (!GetIsInPackage()) {
+        std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
+
+#ifdef _WIN32
+        m_file.open((wchar_t*)path.c_str(), std::basic_ios<char>::in | std::basic_ios<char>::binary);
+#else
+        m_file.open(utf16_to_utf8(path).c_str(), std::basic_ios<char>::in | std::basic_ios<char>::binary);
+#endif
+    }
 }
 
-BaseFileReader::~BaseFileReader() { m_file.close(); }
+BaseFileReader::~BaseFileReader() {
+    if (!GetIsInPackage()) m_file.close();
+}
 
 int64_t BaseFileReader::GetSize() {
     if (m_length < 0) {
+        std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
         assert(!m_file.fail());
         m_file.seekg(0, std::ios_base::end);
         m_length = m_file.tellg();
@@ -23,14 +35,13 @@ int64_t BaseFileReader::GetSize() {
 void BaseFileReader::ReadBytes(std::vector<uint8_t>& buffer, const int64_t count) {
     const auto size = GetSize();
 
-    if (!count) {
+    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
+
+    if (m_position + count > GetSize() || count < 0) {
         buffer.resize(0);
         buffer.clear();
         return;
     }
-
-    assert(0 <= count && count <= size);
-    assert((m_position + count) <= size);
 
     buffer.resize(count);
     m_file.read(reinterpret_cast<char*>(&buffer[0]), count);
@@ -50,7 +61,9 @@ uint64_t BaseFileReader::ReadUInt64() {
     return *reinterpret_cast<const uint64_t*>(buffer.data());
 }
 void BaseFileReader::ReadAllBytes(std::vector<uint8_t>& buffer) {
+    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
     const auto tmp = m_position;
+    m_position = 0;
     ReadBytes(buffer, GetSize());
     m_position = tmp;
 }
@@ -58,6 +71,7 @@ void BaseFileReader::ReadAllBytes(std::vector<uint8_t>& buffer) {
 void BaseFileReader::Seek(const int64_t offset, const SeekOrigin origin) {
     const auto size = GetSize();
 
+    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
     switch (origin) {
         case SeekOrigin::Begin:
             assert(0 <= offset && offset < size);
@@ -79,5 +93,6 @@ void BaseFileReader::Seek(const int64_t offset, const SeekOrigin origin) {
             break;
     }
 }
+
 bool BaseFileReader::GetIsInPackage() const { return false; }
 }  // namespace altseed
