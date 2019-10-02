dc05475edc4a343cdd22c018b8ebf8db7833f630
steLLa-m@gica <s1711399@coins.tsukuba.ac.jp>
2019-10-02 18:35:43 +0900

    Auto stash before merge of "master" and "upstream/io"

--- core/src/IO/BaseFileReader.cpp
+++ core/src/IO/BaseFileReader.cpp
@@ -3,26 +3,14 @@
 #include "../Common/StringHelper.h"
 
 namespace altseed {
-
 BaseFileReader::BaseFileReader(const std::u16string& path) : BaseObject(), m_path(path), m_length(-1), m_position(0) {
-    if (!GetIsInPackage()) {
-        std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
-
-#ifdef _WIN32
-        m_file.open((wchar_t*)path.c_str(), std::basic_ios<char>::in | std::basic_ios<char>::binary);
-#else
-        m_file.open(utf16_to_utf8(path).c_str(), std::basic_ios<char>::in | std::basic_ios<char>::binary);
-#endif
-    }
+    m_file.open(utf16_to_utf8(path), std::basic_ios<char>::in | std::basic_ios<char>::binary);
 }
 
-BaseFileReader::~BaseFileReader() {
-    if (!GetIsInPackage()) m_file.close();
-}
+BaseFileReader::~BaseFileReader() { m_file.close(); }
 
 int64_t BaseFileReader::GetSize() {
     if (m_length < 0) {
-        std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
         assert(!m_file.fail());
         m_file.seekg(0, std::ios_base::end);
         m_length = m_file.tellg();
@@ -35,13 +23,14 @@ int64_t BaseFileReader::GetSize() {
 void BaseFileReader::ReadBytes(std::vector<uint8_t>& buffer, const int64_t count) {
     const auto size = GetSize();
 
-    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
-
-    if (m_position + count > GetSize() || count < 0) {
+    if (!count) {
         buffer.resize(0);
         buffer.clear();
         return;
     }
+
+    assert(0 <= count && count <= size);
+    assert((m_position + count) <= size);
 
     buffer.resize(count);
     m_file.read(reinterpret_cast<char*>(&buffer[0]), count);
@@ -61,9 +50,7 @@ uint64_t BaseFileReader::ReadUInt64() {
     return *reinterpret_cast<const uint64_t*>(buffer.data());
 }
 void BaseFileReader::ReadAllBytes(std::vector<uint8_t>& buffer) {
-    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
     const auto tmp = m_position;
-    m_position = 0;
     ReadBytes(buffer, GetSize());
     m_position = tmp;
 }
@@ -71,7 +58,6 @@ void BaseFileReader::ReadAllBytes(std::vector<uint8_t>& buffer) {
 void BaseFileReader::Seek(const int64_t offset, const SeekOrigin origin) {
     const auto size = GetSize();
 
-    std::unique_lock<std::recursive_mutex> lock(m_readerMtx);
     switch (origin) {
         case SeekOrigin::Begin:
             assert(0 <= offset && offset < size);
@@ -93,6 +79,5 @@ void BaseFileReader::Seek(const int64_t offset, const SeekOrigin origin) {
             break;
     }
 }
-
 bool BaseFileReader::GetIsInPackage() const { return false; }
 }  // namespace altseed
