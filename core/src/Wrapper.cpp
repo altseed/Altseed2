9f431c2dcfa85c1b76dfa16a42fff4ae2aab0974
durswd <swda.durl@gmail.com>
2019-10-19 02:41:28 +0900

    generated wrapper

---
+++ core/src/Wrapper.cpp
@@ -0,0 +1,58 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+  #include <Windows.h>
+#endif
+
+#ifndef CBGEXPORT
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+  #define CBGEXPORT __declspec(dllexport)
+#else
+  #define CBGEXPORT
+#endif
+#endif
+
+#ifndef CBGSTDCALL
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+  #define CBGSTDCALL __stdcall
+#else
+  #define CBGSTDCALL
+#endif
+#endif
+
+
+    #include "Input/Keyboad.h"
+    
+extern "C" {
+
+
+}
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	bool res = true;
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		CoInitializeEx(NULL, COINIT_MULTITHREADED);
+		break;
+	case DLL_PROCESS_DETACH:
+		CoUninitialize();
+		break;
+	case DLL_THREAD_ATTACH:
+		CoInitializeEx(NULL, COINIT_MULTITHREADED);
+		break;
+	case DLL_THREAD_DETACH:
+		CoUninitialize();
+		break;
+	default:
+		break;
+	}
+	return res;
+}
+
+#endif
+
--- scripts/generate_wrapper.py
+++ scripts/generate_wrapper.py
@@ -7,13 +7,12 @@ if __name__ == '__main__':
     # generate
     sharedObjectGenerator = cbg.SharedObjectGenerator(define)
 
-    # TODO: add core header files
     sharedObjectGenerator.header = '''
-    #include "wrapper.h"
+    #include "Input/Keyboad.h"
     '''
 
     sharedObjectGenerator.func_name_create_and_add_shared_ptr = 'Altseed::CreateAndAddSharedPtr'
     sharedObjectGenerator.func_name_add_and_get_shared_ptr = 'Altseed::AddAndGetSharedPtr'
 
-    sharedObjectGenerator.output_path = 'core/src/Wrapper.cpp'
+    sharedObjectGenerator.output_path = '../core/src/Wrapper.cpp'
     sharedObjectGenerator.generate()
