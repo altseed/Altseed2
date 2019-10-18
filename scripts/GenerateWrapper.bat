50780fa36d5994b10927190c5d09fe7824f78ed8
wisteria <wisteria@coins.tsukuba.ac.jp>
2019-10-18 20:58:35 +0900

    update: generate_wrapper

--- scripts/GenerateWrapper.bat
+++
@@ -1,8 +0,0 @@
-cd %~dp0
-FOR /f "usebackq tokens=*" %%i IN (`cd`) DO set PYTHONPATH=%%i;%%i\cbg
-
-cd ..\core\src
-
-python ..\..\scripts\generate_wrapper.py
-
-pause
--- scripts/generate_wrapper.py
+++ scripts/generate_wrapper.py
@@ -1,18 +1,23 @@
-import cbg
-import ctypes
-import sys
+import ctypes, sys, os
 
+repos = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(repos + '/scripts')
+sys.path.append(repos + '/scripts/cbg')
+os.chdir(repos)
+
+import cbg
 from bindings import define
 
 # generate
 sharedObjectGenerator = cbg.SharedObjectGenerator(define)
 
+# TODO: add core header files
 sharedObjectGenerator.header = '''
-// #include "wrapper.h"
+#include "wrapper.h"
 '''
 
 sharedObjectGenerator.func_name_create_and_add_shared_ptr = 'Altseed::CreateAndAddSharedPtr'
 sharedObjectGenerator.func_name_add_and_get_shared_ptr = 'Altseed::AddAndGetSharedPtr'
 
-sharedObjectGenerator.output_path = 'Wrapper.cpp'
+sharedObjectGenerator.output_path = 'core/src/Wrapper.cpp'
 sharedObjectGenerator.generate()
