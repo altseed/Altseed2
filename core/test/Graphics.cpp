c54309e205ff44ca0eacae26b0f8c8fa2cfb01f7
wisteria <wisteria@coins.tsukuba.ac.jp>
2019-11-05 00:03:22 +0900

    fix: typo

--- core/test/Graphics.cpp
+++ core/test/Graphics.cpp
@@ -9,7 +9,7 @@
 
 namespace asd = altseed;
 
-TEST(Grapgics, Initialize) {
+TEST(Graphics, Initialize) {
     EXPECT_TRUE(asd::Core::Initialize(u"test", 1280, 720, asd::CoreOption()));
 
     int count = 0;
@@ -42,7 +42,7 @@ TEST(Grapgics, Initialize) {
     altseed::Graphics::Terminate();
 }
 
-TEST(Grapgics, Texture) {
+TEST(Graphics, Texture) {
     EXPECT_TRUE(asd::Core::Initialize(u"test", 1280, 720, asd::CoreOption()));
 
     int count = 0;
@@ -58,8 +58,8 @@ TEST(Grapgics, Texture) {
 
     {
         int c = 0;
-        for (int x = 0; x <= 3; x++) {
-            for (int y = 0; y <= 3; y++) {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
                 auto sprite = std::make_shared<altseed::Sprite>();
                 sprite->SetMaterial(material);
                 sprite->SetPosition(asd::Vector2DF(x * 120, y * 120));
@@ -70,7 +70,7 @@ TEST(Grapgics, Texture) {
         }
     }
 
-    while (count++ < 1000 && instance->DoEvents()) EXPECT_TRUE(instance->Update());
+    while (count++ < 100 && instance->DoEvents()) EXPECT_TRUE(instance->Update());
 
     altseed::Graphics::Terminate();
 }
