af43787d09e38575767fd040a04f6a9b768c7fe9
GON <hutabasuzukiryu@live.jp>
2019-11-06 14:08:50 +0900

    add: Color

---
+++ core/src/Graphics/Color.cpp
@@ -0,0 +1,19 @@
+#include "Color.h"
+
+namespace altseed {
+Color::Color() : A(255), R(255), G(255), B(255) {}
+
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : A(a), R(r), G(g), B(b) {}
+
+Color Color::operator*(const Color& right) { return Color(R * right.R / 255, G * right.G / 255, B * right.B / 255, A * right.A / 255); }
+
+Color& Color::operator*=(const Color& right) {
+    R = R * right.R / 255;
+    G = G * right.G / 255;
+    B = B * right.B / 255;
+    A = A * right.A / 255;
+    return *this;
+}
+
+bool Color::operator==(const Color& right) { return R == right.R && G == right.G && B == right.B && A == right.A; }
+}  // namespace altseed
