ffe0d085dd5c00fb38870af57272c12774e7a1e4
wisteria <12514442+wistnki@users.noreply.github.com>
2020-01-18 13:29:16 +0900

    Revert "Sound"

--- core/src/Sound/SoundMixer.cpp
+++ core/src/Sound/SoundMixer.cpp
@@ -3,17 +3,20 @@
 namespace altseed
 {
 
-SoundMixer::SoundMixer(bool isReloadingEnabled)
+SoundMixer::SoundMixer(File* file, bool isReloadingEnabled)
 {
 	m_manager = osm::Manager::Create();
+
 	if(m_manager->Initialize())
 	{
 
 	}
 	else
 	{
-		
+		if(m_manager != nullptr) m_manager->Release();
 	}
+
+	SoundSourcesContainer = std::make_shared<ResourceContainer>();
 }
 
 SoundMixer::~SoundMixer()
@@ -25,42 +28,18 @@ SoundMixer::~SoundMixer()
 	}
 }
 
-std::shared_ptr<Sound> SoundMixer::CreateSound(const char16_t* path, bool isDecompressed)
+Sound* SoundMixer::CreateSound(const char16_t* path, bool isDecompressed)
 {
-	if(m_manager == nullptr) return nullptr;
-
-	// Create file instance & null check
-	auto fileInstance = File::GetInstance();
-	if(fileInstance == nullptr) return nullptr;
-
-	// Create static file & null check
-	auto staticFile = fileInstance->CreateStaticFile(path);
-	if(staticFile == nullptr) return nullptr;
-
-	// Get data & Create OSM sound & null check
-	auto sound = m_manager->CreateSound(staticFile->GetData(), staticFile->GetSize(), isDecompressed);
-	if(sound == nullptr)
-	{
-		staticFile->Release();
-		return nullptr;
-	}
-
-	// Create sound & register to container
-	auto resources = Resources::GetInstance();
-	auto soundRet = std::make_shared<Sound>(resources, this, path, sound, isDecompressed);
-	auto soundContainer = resources->GetResourceContainer(ResourceType::Sound);
-	auto soundInfo = std::make_shared<ResourceContainer::ResourceInfomation>(soundRet, path);
-	soundContainer->Register(path, soundInfo);
-
-	return soundRet;
+	return nullptr;
 }
 
-int32_t SoundMixer::Play(std::shared_ptr<Sound> sound)
+int32_t SoundMixer::Play(Sound* sound)
 {
 	if (m_manager == nullptr) return -1;
 	if (sound == nullptr) return -1;
+	auto s = (Sound*)sound;
 
-	return m_manager->Play(sound->GetSound());
+	return m_manager->Play(s->GetSound());
 }
 
 bool SoundMixer::GetIsPlaying(int32_t id)
@@ -165,7 +144,7 @@ float SoundMixer::GetPlaybackPercent(int32_t id)
 	return m_manager->GetPlaybackPercent(id);
 }
 
-void SoundMixer::GetSpectrumData(int32_t id, std::vector<float> &spectrums, int32_t samplingRate, osm::FFTWindow window)
+void SoundMixer::GetSpectrumData(int32_t id, float* spectrums, int32_t samplingRate, osm::FFTWindow window)
 {
 	if (m_manager == nullptr) return;
 	m_manager->GetSpectrumData(id, spectrums, samplingRate, window);
@@ -173,11 +152,7 @@ void SoundMixer::GetSpectrumData(int32_t id, std::vector<float> &spectrums, int3
 
 void SoundMixer::Reload()
 {
-	/*
-	auto resources = Resources::GetInstance();
-	auto container = resources->GetResourceContainer(ResourceType::Sound);
-	for(auto sound : container->GetAllResouces()) sound.second->Reload(0);
-	*/
+	
 }
 
 } // namespace altseed
