88e3c1fcdd651dd73d1ea7d2fec5f15841427004
wraikny <writtenknown@gmail.com>
2020-01-22 02:11:45 +0900

    Add Easing

---
+++ core/src/Math/Easing.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "../BaseObject.h"
+
+namespace Altseed {
+
+enum class EasingType : int32_t
+{
+    Linear,
+    InSine,
+    OutSine,
+    InOutSine,
+    InQuad,
+    OutQuad,
+    InOutQuad,
+    InCubic,
+    OutCubic,
+    InOutCubic,
+    InQuart,
+    OutQuart,
+    InOutQuart,
+    InQuint,
+    OutQuint,
+    InOutQuint,
+    InExpo,
+    OutExpo,
+    InOutExpo,
+    InCirc,
+    OutCirc,
+    InOutCirc,
+    InBack,
+    OutBack,
+    InOutBack,
+    InElastic,
+    OutElastic,
+    InOutElastic,
+    InBounce,
+    OutBounce,
+    InOutBounce,
+};
+
+class Easing {
+public:
+    static float GetEasing(EasingType easing, float t);
+
+    // pseudo method for binding
+    int32_t Release();
+};
+
+
+}
