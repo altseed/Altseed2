774bd38881a70ff115a45be9e2d74594189edf2b
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-07 01:43:00 +0900

    外枠作成

--- scripts/bindings/common.py
+++ scripts/bindings/common.py
@@ -6,6 +6,8 @@ VoidPtr = cbg.Class('Altseed', 'VoidPtr')
 
 SeekOrigin = cbg.Enum('Altseed', 'SeekOrigin')
 with SeekOrigin as enum_:
+    enum_.brief = cbg.Description()
+    enum_.brief.add('ja', '')
     enum_.add('Begin')
     enum_.add('Current')
     enum_.add('End')
@@ -13,11 +15,19 @@ with SeekOrigin as enum_:
 Int8Array = cbg.Class('Altseed', 'Int8Array')
 with Int8Array as class_:
     with class_.add_func("CopyTo") as func:
-        func.add_arg(Int8Array, "array")
-        func.add_arg(int, "size")
+        func.brief = cbg.Description()
+        func.brief.add('ja', '指定したインスタンスにデータをコピーする')
+        with func.add_arg(Int8Array, "array") as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', 'コピー先のインスタンス')
+        with func.add_arg(int, "size") as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', 'コピーするデータ量')
 
 ResourceType = cbg.Enum('Altseed', 'ResourceType')
 with ResourceType as enum_:
+    enum_.brief = cbg.Description()
+    enum_.brief.add('ja', 'リソースのタイプを表す')
     enum_.add('StaticFile')
     enum_.add('StreamFile')
     enum_.add('Texture2D')
@@ -27,11 +37,21 @@ with ResourceType as enum_:
 Resources = cbg.Class('Altseed', 'Resources')
 with Resources as class_:
     with class_.add_func('GetInstance') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.is_static = True
         func.return_value.type_ = Resources
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetResourcesCount') as func:
-        func.add_arg(ResourceType, 'type')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ResourceType, 'type') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     class_.add_func('Clear')
     class_.add_func('Reload')
 
--- scripts/bindings/io.py
+++ scripts/bindings/io.py
@@ -7,66 +7,174 @@ from .common import *
 StaticFile = cbg.Class('Altseed', 'StaticFile')
 with StaticFile as class_:
     with class_.add_func('GetBuffer') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = Int8Array
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetPath') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = ctypes.c_wchar_p
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetSize') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetIsInPackage') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('Reload') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
 
 StreamFile = cbg.Class('Altseed', 'StreamFile')
 with StreamFile as class_:
     with class_.add_func('GetSize') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetCurrentPosition') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('Read') as func:
-        func.add_arg(int, 'size')
+        with func.add_arg(int, 'size') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetTempBuffer') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = Int8Array
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetTempBufferSize') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = int
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('GetIsInPackage') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('Reload') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
 
 File = cbg.Class('Altseed', 'File')
 with File as class_:
     with class_.add_func('GetInstance') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
         func.is_static = True
         func.return_value.type_ = File
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('CreateStaticFile') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = StaticFile
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('CreateStreamFile') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = StreamFile
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('AddRootDirectory') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('AddRootPackageWithPassword') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
-        func.add_arg(ctypes.c_wchar_p, 'password')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'password') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('AddRootPackage') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
-    class_.add_func('ClearRootDirectories')
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
+    with class_.add_func('ClearRootDirectories') as func:
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
     with class_.add_func('Exists') as func:
-        func.add_arg(ctypes.c_wchar_p, 'path')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'path') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('Pack') as func:
-        func.add_arg(ctypes.c_wchar_p, 'srcPath')
-        func.add_arg(ctypes.c_wchar_p, 'dstPath')
+        with func.add_arg(ctypes.c_wchar_p, 'srcPath') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'dstPath') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
     with class_.add_func('PackWithPassword') as func:
-        func.add_arg(ctypes.c_wchar_p, 'srcPath')
-        func.add_arg(ctypes.c_wchar_p, 'dstPath')
-        func.add_arg(ctypes.c_wchar_p, 'password')
+        func.brief = cbg.Description()
+        func.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'srcPath') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'dstPath') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
+        with func.add_arg(ctypes.c_wchar_p, 'password') as arg:
+            arg.brief = cbg.Description()
+            arg.brief.add('ja', '')
         func.return_value.type_ = bool
+        func.return_value.brief = cbg.Description()
+        func.return_value.brief.add('ja', '')
         
