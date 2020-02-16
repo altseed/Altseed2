7d943ac65b5d55e1ad6f55225b46732e84292dec
durswd <swda.durl@gmail.com>
2020-02-17 01:53:55 +0900

    Fix test, add assertion

--- core/test/Log.cpp
+++ core/test/Log.cpp
@@ -1,4 +1,4 @@
-#include <Common/StringHelper.h>
+﻿#include <Common/StringHelper.h>
 #include <Logger/Log.h>
 #include <gtest/gtest.h>
 
@@ -19,31 +19,31 @@ TEST(Log, Write) {
     EXPECT_TRUE(Altseed::Log::Initialize(log_filename));
 
     const auto categories = {
-        Altseed::LogCategory::Core,
-        Altseed::LogCategory::Engine,
-        Altseed::LogCategory::User,
+            Altseed::LogCategory::Core,
+            Altseed::LogCategory::Engine,
+            Altseed::LogCategory::User,
     };
 
     const auto levels = {
-        Altseed::LogLevel::Trace,
-        Altseed::LogLevel::Debug,
-        Altseed::LogLevel::Info,
-        Altseed::LogLevel::Warning,
-        Altseed::LogLevel::Error,
-        Altseed::LogLevel::Critical,
-        Altseed::LogLevel::Off,
+            Altseed::LogLevel::Trace,
+            Altseed::LogLevel::Debug,
+            Altseed::LogLevel::Info,
+            Altseed::LogLevel::Warning,
+            Altseed::LogLevel::Error,
+            Altseed::LogLevel::Critical,
+            Altseed::LogLevel::Off,
     };
 
     for (const auto category : categories) {
         for (const auto level : levels) {
             Altseed::Log::GetInstance()->Write(
-                category,
-                level,
-                u"caetgory:{0}, level:{1}, nyan:{2}, にゃーん:{3}",
-                static_cast<int>(category),
-                static_cast<int>(level),
-                "nyan",
-                "にゃーん");
+                    category,
+                    level,
+                    u"caetgory:{0}, level:{1}, nyan:{2}, にゃーん:{3}",
+                    static_cast<int>(category),
+                    static_cast<int>(level),
+                    "nyan",
+                    "にゃーん");
         }
     }
 
