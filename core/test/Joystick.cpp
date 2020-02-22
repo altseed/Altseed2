4e56b3a341c7aac5ad26b7bbf95d02260338ae31
k-usuha <kouta528bado@yahoo.co.jp>
2020-02-22 18:33:34 +0900

    Update Joystick test

--- core/test/Joystick.cpp
+++ core/test/Joystick.cpp
@@ -1,4 +1,4 @@
-﻿#include <Core.h>
+#include <Core.h>
 #include <Input/Joystick.h>
 #include <gtest/gtest.h>
 #include <hidapi.h>
@@ -12,43 +12,14 @@ TEST(Joystick, Initialize) {
 
     EXPECT_TRUE(Altseed::Core::Initialize(s16, 640, 480, Altseed::Configuration::Create()));
 
-    //    Altseed::JoystickManager::GetInstance()->PrintDevicesInfo();
-    //    temp();
-    Altseed::Joystick* joystick = new Altseed::Joystick();
+//    std::shared_ptr<Altseed::Joystick> joystick = std::make_shared<Altseed::Joystick>(Altseed::Joystick());
+    auto joystick = new Altseed::Joystick();
+
     joystick->Initialize(Altseed::JoystickType::JoyconL);
     for (int i = 0; i < 10; i++) {
-        printf("name: %ls", joystick->GetJoystickName(i));
-    }
-
-    std::chrono::system_clock::time_point start, end;  // 型は auto で可
-    start = std::chrono::system_clock::now();  // 計測開始時間
-    // 処理
-    end = std::chrono::system_clock::now();  // 計測終了時間
-    joystick->SetVibration(0, 462.457855f, 462.457855f, 0.981378f, 0.981378f, 1000);
-
-    //    joystick->SetVibration(0, 600.0f, 200.0f, 0.8f, 0.8f, 3000);
-
-    //    float pre_h = 0;
-    //    float pre_v = 0;
-    //
-    while (std::chrono::duration_cast<std::chrono::seconds>(end - start).count() <= 5.0f) {
-        joystick->RefreshVibrateState();
-
-        joystick->RefreshInputState();
-        //        float h = joystick->GetAxisState(0, Altseed::JoystickAxisType::LeftH);
-        //        float v = joystick->GetAxisState(0, Altseed::JoystickAxisType::LeftV);
-        //
-        //        if (pre_h == h && pre_v == v) {
-        //
-        //        } else {
-        //            printf("\nh: %f\n", h);
-        //            printf("v: %f\n", v);
-        //        }
-        //
-        //        pre_h = h;
-        //        pre_v = v;
-
-        end = std::chrono::system_clock::now();  // 計測終了時間
+        if (joystick->IsPresent(i))
+            printf("name: %s", joystick->GetJoystickName(i).c_str());
+        
     }
 
     Altseed::Core::Terminate();
