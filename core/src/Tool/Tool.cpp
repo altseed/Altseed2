3a013920397b242c5dde2cd4de06246611b41c88
wraikny <writtenknown@gmail.com>
2020-03-02 01:49:20 +0900

    Add Tool implements

--- core/src/Tool/Tool.cpp
+++ core/src/Tool/Tool.cpp
@@ -84,10 +84,261 @@ void Tool::Render() {
     platform_->RenderDrawData(ImGui::GetDrawData(), Graphics::GetInstance()->GetCommandList()->GetLL());
 }
 
-bool Tool::Begin(const char16_t* name) { return ImGui::Begin(utf16_to_utf8(name).c_str(), nullptr); }
+inline ImVec2 toImVec2(const Vector2F& v) { return ImVec2(v.X, v.Y); }
 
-void Tool::Text(const char16_t* text) { ImGui::Text(utf16_to_utf8(text).c_str()); }
+inline ImVec4 toImVec4(const Vector4F& v) { return ImVec4(v.X, v.Y, v.Z, v.W); }
+
+bool Tool::Begin(const char16_t* name, ToolWindow flags) { return ImGui::Begin(utf16_to_utf8(name).c_str(), nullptr, static_cast<ImGuiWindowFlags>(flags)); }
 
 void Tool::End() { ImGui::End(); }
+
+void Tool::Dummy(Vector2F size) { ImGui::Dummy(toImVec2(size)); }
+
+void Tool::Text(const char16_t* text) { ImGui::Text("%s", utf16_to_utf8(text).c_str()); }
+
+void Tool::TextUnformatted(const char16_t* text) { ImGui::TextUnformatted(utf16_to_utf8(text).c_str()); }
+
+void Tool::TextWrapped(const char16_t* text) { ImGui::TextWrapped("%s", utf16_to_utf8(text).c_str());  };
+
+void Tool::TextColored(const Vector4F color, const char16_t* text) { ImGui::TextColored(toImVec4(color), "%s", utf16_to_utf8(text).c_str()); }
+
+void Tool::TextDisabled(const char16_t* text) { ImGui::TextDisabled("%s", utf16_to_utf8(text).c_str()); }
+
+void Tool::BulletText(const char16_t* text) { ImGui::BulletText("%s", utf16_to_utf8(text).c_str()); }
+
+void Tool::LabelText(const char16_t* label, const char16_t* text) { ImGui::LabelText(utf16_to_utf8(label).c_str(), "%s", utf16_to_utf8(text).c_str()); }
+
+bool Tool::CollapsingHeader(const char16_t* label, ToolTreeNode flags) { return ImGui::CollapsingHeader(utf16_to_utf8(label).c_str(), static_cast<ImGuiTreeNodeFlags>(flags)); }
+
+bool Tool::TreeNode(const char16_t* label) { return ImGui::TreeNode(utf16_to_utf8(label).c_str()); }
+
+bool Tool::TreeNodeEx(const char16_t* label, ToolTreeNode flags) { return ImGui::TreeNodeEx(utf16_to_utf8(label).c_str(), static_cast<ImGuiTreeNodeFlags>(flags)); }
+
+void Tool::TreePop() { ImGui::TreePop(); }
+
+void Tool::SetNextItemOpen(bool is_open, ToolCond cond) { ImGui::SetNextItemOpen(is_open, static_cast<ImGuiCond>(cond)); }
+
+bool Tool::Button(const char16_t* label, const Vector2F size) { return ImGui::Button(utf16_to_utf8(label).c_str(), toImVec2(size)); }
+
+bool Tool::CheckBox(const char16_t* label, bool* is_open) { return ImGui::Checkbox(utf16_to_utf8(label).c_str(), is_open); }
+
+bool Tool::RadioButton(const char16_t* label, bool active) { return ImGui::RadioButton(utf16_to_utf8(label).c_str(), active); }
+
+bool Tool::ArrowButton(const char16_t* id, ToolDir dir) { return ImGui::ArrowButton(utf16_to_utf8(id).c_str(), static_cast<ImGuiDir>(dir)); }
+
+bool Combo(const char16_t* label, int* current, const char16_t* items_separated_by_zeros) {
+    return ImGui::Combo(utf16_to_utf8(label).c_str(), current, utf16_to_utf8(items_separated_by_zeros).c_str());
+}
+
+bool ListBox(const char16_t* label, int* current, const char* items[], size_t count) {
+    return ImGui::ListBox(utf16_to_utf8(label).c_str(), current, items, count);
+}
+
+bool Tool::InvisibleButton(const char16_t* label, Vector2F size) {
+    return ImGui::InvisibleButton(utf16_to_utf8(label).c_str(), toImVec2(size));
+}
+
+bool Tool::Selectable(const char16_t* label, bool* selected, ToolSelectable flags) {
+    return ImGui::Selectable(utf16_to_utf8(label).c_str(), selected, static_cast<ImGuiSelectableFlags>(flags));
+}
+
+bool Tool::InputText(const char16_t* label, char* buf, size_t buf_size, ToolInputText flags) {
+    return ImGui::InputText(utf16_to_utf8(label).c_str(), buf, buf_size, static_cast<ImGuiInputTextFlags>(flags));
+}
+
+bool Tool::InputTextWithHint(const char16_t* label, const char16_t* hint, char* buf, size_t buf_size, ToolInputText flags) {
+    return ImGui::InputTextWithHint(utf16_to_utf8(label).c_str(), utf16_to_utf8(hint).c_str(), buf, buf_size, static_cast<ImGuiInputTextFlags>(flags));
+}
+
+bool Tool::InputTextMultiline(const char16_t* label, char* buf, size_t buf_size, Vector2F size, ToolInputText flags) {
+    return ImGui::InputTextMultiline(utf16_to_utf8(label).c_str(), buf, buf_size, toImVec2(size), static_cast<ImGuiInputTextFlags>(flags));
+}
+
+bool Tool::InputInt(const char16_t* label, int* v) { return ImGui::InputInt(utf16_to_utf8(label).c_str(), v); }
+
+bool Tool::InputInt2(const char16_t* label, int* array) { return ImGui::InputInt2(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::InputInt3(const char16_t* label, int* array) { return ImGui::InputInt3(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::InputInt4(const char16_t* label, int* array) { return ImGui::InputInt4(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::InputFloat(const char16_t* label, float* v) { return ImGui::InputFloat(utf16_to_utf8(label).c_str(), v); }
+
+bool Tool::InputFloat2(const char16_t* label, float* array) { return ImGui::InputFloat2(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::InputFloat3(const char16_t* label, float* array) { return ImGui::InputFloat3(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::InputFloat4(const char16_t* label, float* array) { return ImGui::InputFloat4(utf16_to_utf8(label).c_str(), array); }
+
+bool Tool::DragInt(const char16_t* label, int* v, float speed, int v_min, int v_max) {
+    return ImGui::DragInt(utf16_to_utf8(label).c_str(), v, speed, v_min, v_max);
+}
+
+bool Tool::SliderInt(const char16_t* label, int* v, float speed, int v_min, int v_max) {
+    return ImGui::SliderInt(utf16_to_utf8(label).c_str(), v, v_min, v_max);
+}
+
+bool Tool::SliderInt2(const char16_t* label, int* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderInt2(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderInt3(const char16_t* label, int* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderInt3(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderInt4(const char16_t* label, int* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderInt4(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderFloat(const char16_t* label, float* v, float speed, int v_min, int v_max) {
+    return ImGui::SliderFloat(utf16_to_utf8(label).c_str(), v, v_min, v_max);
+}
+
+bool Tool::SliderFloat2(const char16_t* label, float* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderFloat2(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderFloat3(const char16_t* label, float* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderFloat3(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderFloat4(const char16_t* label, float* array, float speed, int v_min, int v_max) {
+    return ImGui::SliderFloat4(utf16_to_utf8(label).c_str(), array, v_min, v_max);
+}
+
+bool Tool::SliderAngle(const char16_t* label, float* angle) { return ImGui::SliderAngle(utf16_to_utf8(label).c_str(), angle); }
+
+bool Tool::VSliderInt(const char16_t* label, Vector2F size, int* v, float v_min, float v_max) {
+    return ImGui::VSliderInt(utf16_to_utf8(label).c_str(), toImVec2(size), v, v_min, v_max);
+}
+
+bool Tool::VSliderFloat(const char16_t* label, Vector2F size, float* v, float v_min, float v_max) {
+    return ImGui::VSliderFloat(utf16_to_utf8(label).c_str(), toImVec2(size), v, v_min, v_max);
+}
+
+bool Tool::DragFloat(const char16_t* label, float* v, float speed, int v_min, int v_max) {
+    return ImGui::DragFloat(utf16_to_utf8(label).c_str(), v, speed, v_min, v_max);
+}
+bool Tool::DragIntRange2(const char16_t* label, int* current_min, int* current_max, float speed, int v_min, int v_max) {
+    return ImGui::DragIntRange2(utf16_to_utf8(label).c_str(), current_min, current_max, speed, v_min, v_max);
+}
+
+bool Tool::DragFloatRange2(const char16_t* label, float* current_min, float* current_max, float speed, int v_min, int v_max) {
+    return ImGui::DragFloatRange2(utf16_to_utf8(label).c_str(), current_min, current_max, speed, v_min, v_max);
+}
+
+bool Tool::ColorEdit3(const char16_t* label, float* color, ToolColorEdit flags) {
+    return ImGui::ColorEdit3(utf16_to_utf8(label).c_str(), color, static_cast<ImGuiColorEditFlags>(flags));
+}
+
+bool Tool::ColorEdit4(const char16_t* label, float* color, ToolColorEdit flags) {
+    return ImGui::ColorEdit4(utf16_to_utf8(label).c_str(), color, static_cast<ImGuiColorEditFlags>(flags));
+}
+
+bool Tool::BeginPopup(const char16_t* label) { return ImGui::BeginPopup(utf16_to_utf8(label).c_str()); }
+
+bool Tool::BeginPopupModal(const char16_t* label) { return ImGui::BeginPopupModal(utf16_to_utf8(label).c_str()); }
+
+void Tool::EndPopup() { ImGui::EndPopup(); }
+
+bool Tool::BeginChild(const char16_t* label, Vector2F size, bool border, ToolWindow flags) {
+    return ImGui::BeginChild(utf16_to_utf8(label).c_str(), toImVec2(size), border, static_cast<ImGuiWindowFlags>(flags));
+}
+
+void Tool::EndChild() { ImGui::EndChild(); }
+
+bool Tool::BeginMenuBar() { return ImGui::BeginMenuBar(); }
+
+void Tool::EndMenuBar() { ImGui::EndMenuBar(); }
+
+bool Tool::BeginMenu(const char16_t* label, bool enabled) { return ImGui::BeginMenu(utf16_to_utf8(label).c_str(), enabled); }
+
+void Tool::EndMenu() { ImGui::EndMenu(); }
+
+bool Tool::MenuItem(const char16_t* label, const char16_t* shortcut, bool selected, bool enabled) {
+    return ImGui::MenuItem(utf16_to_utf8(label).c_str(), utf16_to_utf8(shortcut).c_str(), selected, enabled);
+}
+
+bool Tool::BeginTabBar(const char16_t* id, ToolTabBar flags) {
+    return ImGui::BeginTabBar(utf16_to_utf8(id).c_str(), static_cast<ImGuiTabBarFlags>(flags));
+}
+
+void Tool::EndTabBar() { ImGui::EndTabBar(); }
+
+bool Tool::BeginTabItem(const char16_t* label) {
+    return ImGui::BeginTabItem(utf16_to_utf8(label).c_str());
+}
+
+bool Tool::EndTabItem() { ImGui::EndTabItem(); }
+
+void Tool::Indent() { ImGui::Indent(); }
+
+void Tool::Unindent() { ImGui::Unindent(); }
+
+void Tool::Separator() { ImGui::Separator(); }
+
+void Tool::SetToolTip(const char16_t* text) { ImGui::SetTooltip("%s", utf16_to_utf8(text).c_str()); }
+
+void Tool::BeginTooltip() { ImGui::BeginTooltip(); }
+
+void Tool::EndTooltip() { ImGui::EndTooltip(); }
+
+void Tool::NewLine() { ImGui::NewLine(); }
+
+void Tool::SameLine() { ImGui::SameLine(); }
+
+void Tool::PushTextWrapPos(float wrap_local_pos_x) { ImGui::PushTextWrapPos(wrap_local_pos_x); }
+
+void Tool::PopTextWrapPos() { ImGui::PopTextWrapPos(); }
+
+void Tool::SetNextItemWidth(int width) { ImGui::SetNextItemWidth(width); }
+
+void Tool::PushButtonRepeat(bool repeat) { ImGui::PushButtonRepeat(repeat); }
+
+void Tool::PopButtonRepeat() { ImGui::PopButtonRepeat(); }
+
+void Tool::Columns(int count, bool border) { ImGui::Columns(count, static_cast<const char*>(__null), border); }
+
+void Tool::PushID(int id) { ImGui::PushID(id); }
+
+void Tool::PopID() { ImGui::PopID(); }
+
+bool Tool::IsItemActive() { return ImGui::IsItemActive(); }
+
+bool Tool::IsItemHovered() { return ImGui::IsItemHovered(); }
+
+void Tool::SetScrollHere() { ImGui::SetScrollHere(); }
+
+void Tool::SetScrollHereX() { ImGui::SetScrollHereX(); }
+
+void Tool::SetScrollHereY() { ImGui::SetScrollHereY(); }
+
+float Tool::GetTextLineHeight() { return ImGui::GetTextLineHeight(); }
+
+float Tool::GetFontSize() { return ImGui::GetFontSize(); }
+
+Vector2F Tool::GetWindowSize() {
+    auto size = ImGui::GetWindowSize();
+    return Vector2F(size.x, size.y);
+}
+
+void Tool::SetWindowSize(Vector2F size) { ImGui::SetWindowSize(toImVec2(size)); }
+
+bool Tool::IsMousePosValid() { return ImGui::IsMousePosValid(); }
+
+bool Tool::IsMouseDragging() { return ImGui::IsMouseDragging(); }
+
+bool Tool::IsMouseDoubleClicked(int button) {
+    return ImGui::IsMouseDoubleClicked(button);
+}
+
+Vector2F Tool::GetMouseDragDelta(int button) {
+    auto d = ImGui::GetMouseDragDelta(button);
+    return Vector2F(d.x, d.y);
+}
+
+void Tool::ResetMouseDragDelta(int button) { ImGui::ResetMouseDragDelta(button); }
+
+void Tool::SetNextWindowContentSize(Vector2F size) { ImGui::SetNextWindowContentSize(toImVec2(size)); }
 
 }  // namespace Altseed
--- core/src/Tool/Tool.h
+++ core/src/Tool/Tool.h
@@ -1,12 +1,149 @@
 #pragma once
 
 #include "../Graphics/Graphics.h"
+#include "../Math/Vector2F.h"
+#include "../Math/Vector4F.h"
 
 class ImguiPlatform;
 
 namespace Altseed {
 
 class Graphics;
+
+enum class ToolDir : int32_t {
+    None = -1,
+    Left = 0,
+    Right = 1,
+    Up = 2,
+    Down = 3,
+};
+
+enum class ToolCond : int32_t {
+    None = 0,
+    Always = 1 << 0,
+    Once = 1 << 1,
+    FirstUseEver = 1 << 2,
+    Appearing = 1 << 3,
+};
+
+enum class ToolTreeNode : int32_t {
+    None = 0,
+    Selected = 1 << 0,
+    Framed = 1 << 1,
+    AllowItemOverlap = 1 << 2,
+    NoTreePushOnOpen = 1 << 3,
+    NoAutoOpenOnLog = 1 << 4,
+    DefaultOpen = 1 << 5,
+    OpenOnDoubleClick = 1 << 6,
+    OpenOnArrow = 1 << 7,
+    Leaf = 1 << 8,
+    Bullet = 1 << 9,
+    FramePadding = 1 << 10,
+    SpanAvailWidth = 1 << 11,
+    SpanFullWidth = 1 << 12,
+    NavLeftJumpsBackHere = 1 << 13,
+    CollapsingHeader = Framed | NoTreePushOnOpen | NoAutoOpenOnLog
+};
+
+enum class ToolInputText : int32_t {
+    None = 0,
+    CharsDecimal = 1 << 0,
+    CharsHexadecimal = 1 << 1,
+    CharsUppercase = 1 << 2,
+    CharsNoBlank = 1 << 3,
+    AutoSelectAll = 1 << 4,
+    EnterReturnsTrue = 1 << 5,
+    CallbackCompletion = 1 << 6,
+    CallbackHistory = 1 << 7,
+    CallbackAlways = 1 << 8,
+    CallbackCharFilter = 1 << 9,
+    AllowTabInput = 1 << 10,
+    CtrlEnterForNewLine = 1 << 11,
+    NoHorizontalScroll = 1 << 12,
+    AlwaysInsertMode = 1 << 13,
+    ReadOnly = 1 << 14,
+    Password = 1 << 15,
+    NoUndoRedo = 1 << 16,
+    CharsScientific = 1 << 17,
+    CallbackResize = 1 << 18,
+};
+
+enum class ToolColorEdit : int32_t {
+    None = 0,
+    NoAlpha = 1 << 1,
+    NoPicker = 1 << 2,
+    NoOptions = 1 << 3,
+    NoSmallPreview = 1 << 4,
+    NoInputs = 1 << 5,
+    NoTooltip = 1 << 6,
+    NoLabel = 1 << 7,
+    NoSidePreview = 1 << 8,
+    NoDragDrop = 1 << 9,
+    AlphaBar = 1 << 16,
+    AlphaPreview = 1 << 17,
+    AlphaPreviewHalf = 1 << 18,
+    HDR = 1 << 19,
+    DisplayRGB = 1 << 20,
+    DisplayHSV = 1 << 21,
+    DisplayHex = 1 << 22,
+    Uint8 = 1 << 23,
+    Float = 1 << 24,
+    PickerHueBar = 1 << 25,
+    PickerHueWheel = 1 << 26,
+    InputRGB = 1 << 27,
+    InputHSV = 1 << 28,
+    OptionsDefault = Uint8 | DisplayRGB | InputRGB | PickerHueBar,
+};
+
+enum class ToolSelectable {
+    None = 0,
+    DontClosePopups = 1 << 0,
+    SpanAllColumns = 1 << 1,
+    AllowDoubleClick = 1 << 2,
+    Disabled = 1 << 3,
+    AllowItemOverlap = 1 << 4,
+};
+
+enum class ToolWindow : int32_t {
+    None = 0,
+    NoTitleBar = 1 << 0,
+    NoResize = 1 << 1,
+    NoMove = 1 << 2,
+    NoScrollbar = 1 << 3,
+    NoScrollWithMouse = 1 << 4,
+    NoCollapse = 1 << 5,
+    AlwaysAutoResize = 1 << 6,
+    NoBackground = 1 << 7,
+    NoSavedSettings = 1 << 8,
+    NoMouseInputs = 1 << 9,
+    MenuBar = 1 << 10,
+    HorizontalScrollbar = 1 << 11,
+    NoFocusOnAppearing = 1 << 12,
+    NoBringToFrontOnFocus = 1 << 13,
+    AlwaysVerticalScrollbar = 1 << 14,
+    AlwaysHorizontalScrollbar = 1 << 15,
+    AlwaysUseWindowPadding = 1 << 16,
+    NoNavInputs = 1 << 18,
+    NoNavFocus = 1 << 19,
+    UnsavedDocument = 1 << 20,
+    NoNav = NoNavInputs | NoNavFocus,
+    NoDecoration = NoTitleBar | NoResize | NoScrollbar | NoCollapse,
+    NoInputs = NoMouseInputs | NoNavInputs | NoNavFocus,
+};
+
+enum class ToolTabBar : int32_t {
+    None = 0,
+    Reorderable = 1 << 0,
+    AutoSelectNewTabs = 1 << 1, 
+    TabListPopupButton = 1 << 2,
+    NoCloseWithMiddleMouseButton = 1 << 3,
+    NoTabListScrollingButtons = 1 << 4,
+    NoTooltip = 1 << 5,
+    FittingPolicyResizeDown = 1 << 6,
+    FittingPolicyScroll = 1 << 7,
+    FittingPolicyMask = FittingPolicyResizeDown | FittingPolicyScroll,
+    FittingPolicyDefault = FittingPolicyResizeDown,
+};
 
 class Tool : public BaseObject {
 private:
@@ -28,11 +165,197 @@ public:
 
     void Render();
 
-    bool Begin(const char16_t* name);
+    bool Begin(const char16_t* name, ToolWindow flags = ToolWindow::None);
+
+    void End();
+
+    void Dummy(Vector2F size);
 
     void Text(const char16_t* text);
 
-    void End();
+    void TextUnformatted(const char16_t* text);
+
+    void TextWrapped(const char16_t* text);
+
+    void TextColored(const Vector4F color, const char16_t* text);
+
+    void TextDisabled(const char16_t* text);
+
+    void BulletText(const char16_t* text);
+
+    void LabelText(const char16_t* label, const char16_t* text);
+
+    bool CollapsingHeader(const char16_t* label, ToolTreeNode flags = ToolTreeNode::None);
+
+    bool TreeNode(const char16_t* label);
+
+    bool TreeNodeEx(const char16_t* label, ToolTreeNode flags = ToolTreeNode::None);
+
+    void TreePop();
+
+    void SetNextItemOpen(bool is_open, ToolCond cond = ToolCond::None);
+
+    bool Button(const char16_t* label, const Vector2F size);
+
+    bool CheckBox(const char16_t* label, bool* is_open);
+
+    bool RadioButton(const char16_t* label, bool active);
+
+    bool ArrowButton(const char16_t* id, ToolDir dir);
+
+    bool InvisibleButton(const char16_t* label, Vector2F size);
+
+    bool Combo(const char16_t* label, int* current, const char16_t* items_separated_by_zeros);
+
+    bool ListBox(const char16_t* label, int* current, const char* items[], size_t count);
+
+    bool Selectable(const char16_t* label, bool* selected, ToolSelectable flags = ToolSelectable::None);
+
+    bool InputText(const char16_t* label, char* buf, size_t buf_size, ToolInputText flags = ToolInputText::None);
+
+    bool InputTextWithHint(const char16_t* label, const char16_t* hint, char* buf, size_t buf_size, ToolInputText flags = ToolInputText::None);
+
+    bool InputTextMultiline(const char16_t* label, char* buf, size_t buf_size, Vector2F size, ToolInputText flags = ToolInputText::None);
+
+    bool InputInt(const char16_t* label, int* v);
+
+    bool InputInt2(const char16_t* label, int* array);
+
+    bool InputInt3(const char16_t* label, int* array);
+
+    bool InputInt4(const char16_t* label, int* array);
+
+    bool InputFloat(const char16_t* label, float* v);
+
+    bool InputFloat2(const char16_t* label, float* array);
+
+    bool InputFloat3(const char16_t* label, float* array);
+
+    bool InputFloat4(const char16_t* label, float* array);
+
+    bool SliderInt(const char16_t* label, int* v, float speed, int v_min, int v_max);
+
+    bool SliderInt2(const char16_t* label, int* array, float speed, int v_min, int v_max);
+
+    bool SliderInt3(const char16_t* label, int* array, float speed, int v_min, int v_max);
+
+    bool SliderInt4(const char16_t* label, int* array, float speed, int v_min, int v_max);
+
+    bool SliderFloat(const char16_t* label, float* v, float speed, int v_min, int v_max);
+
+    bool SliderFloat2(const char16_t* label, float* array, float speed, int v_min, int v_max);
+
+    bool SliderFloat3(const char16_t* label, float* array, float speed, int v_min, int v_max);
+
+    bool SliderFloat4(const char16_t* label, float* array, float speed, int v_min, int v_max);
+
+    bool SliderAngle(const char16_t* label, float* angle);
+
+    bool VSliderInt(const char16_t* label, Vector2F size, int* v, float v_min, float v_max);
+
+    bool VSliderFloat(const char16_t* label, Vector2F size, float* v, float v_min, float v_max);
+
+    bool DragInt(const char16_t* label, int* v, float speed, int v_min, int v_max);
+
+    bool DragFloat(const char16_t* label, float* v, float speed, int v_min, int v_max);
+
+    bool DragIntRange2(const char16_t* label, int* current_min, int* current_max, float speed, int v_min, int v_max);
+
+    bool DragFloatRange2(const char16_t* label, float* current_min, float* current_max, float speed, int v_min, int v_max);
+
+    bool ColorEdit3(const char16_t* label, float* color, ToolColorEdit flags = ToolColorEdit::None);
+
+    bool ColorEdit4(const char16_t* label, float* color, ToolColorEdit flags = ToolColorEdit::None);
+    
+    bool BeginPopup(const char16_t* label);
+
+    bool BeginPopupModal(const char16_t* label);
+
+    void EndPopup();
+
+    bool BeginChild(const char16_t* label, Vector2F size, bool border, ToolWindow flags = ToolWindow::None);
+
+    void EndChild();
+
+    bool BeginMenuBar();
+
+    void EndMenuBar();
+
+    bool BeginMenu(const char16_t* label, bool enabled);
+
+    void EndMenu();
+
+    bool MenuItem(const char16_t* label, const char16_t* shortcut, bool selected, bool enabled);
+
+    bool BeginTabBar(const char16_t* label, ToolTabBar flags = ToolTabBar::None);
+
+    void EndTabBar();
+
+    bool BeginTabItem(const char16_t* label);
+
+    bool EndTabItem();
+
+    void Indent();
+
+    void Unindent();
+
+    void Separator();
+
+    void SetToolTip(const char16_t* text);
+
+    void BeginTooltip();
+
+    void EndTooltip();
+
+    void NewLine();
+
+    void SameLine();
+
+    void PushTextWrapPos(float wrap_local_pos_x);
+
+    void PopTextWrapPos();
+
+    void SetNextItemWidth(int width);
+
+    void PushButtonRepeat(bool repeat);
+
+    void PopButtonRepeat();
+
+    void Columns(int count,  bool border);
+
+    void PushID(int id);
+
+    void PopID();
+
+    bool IsItemActive();
+
+    bool IsItemHovered();
+
+    void SetScrollHere();
+
+    void SetScrollHereX();
+
+    void SetScrollHereY();
+
+    float GetTextLineHeight();
+
+    float GetFontSize();
+    
+    Vector2F GetWindowSize();
+
+    void SetWindowSize(Vector2F size);
+
+    bool IsMousePosValid();
+
+    bool IsMouseDragging();
+
+    bool IsMouseDoubleClicked(int id);
+
+    Vector2F GetMouseDragDelta(int button);
+
+    void ResetMouseDragDelta(int button);
+
+    void SetNextWindowContentSize(Vector2F size);
 };
 
 }  // namespace Altseed
