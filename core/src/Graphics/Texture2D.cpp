dfde2662996b32affc3c08c5044a4632a485052c
GON <hutabasuzukiryu@live.jp>
2020-04-05 03:20:54 +0900

    fix: cache

--- core/src/Graphics/Texture2D.cpp
+++ core/src/Graphics/Texture2D.cpp
@@ -38,10 +38,8 @@ std::shared_ptr<Texture2D> Texture2D::Load(const char16_t* path) {
     std::lock_guard<std::mutex> lock(locked.Get());
 
     auto resources = Resources::GetInstance();
-    // TODO
     auto cache = std::dynamic_pointer_cast<Texture2D>(resources->GetResourceContainer(ResourceType::Texture2D)->Get(path));
     if (cache != nullptr) {
-        cache->AddRef();
         return cache;
     }
 
