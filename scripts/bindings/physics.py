ef0682afbacbde4e56febb90cd0988de4da2d62e
wisteria <wisnkcs@gmail.com>
2020-04-08 00:29:26 +0900

    fix collider bindings

--- scripts/bindings/physics.py
+++ scripts/bindings/physics.py
@@ -8,26 +8,30 @@ Collider = cbg.Class('Altseed', 'Collider')
 with Collider as class_:
     with class_.add_constructor() as func_:
         func_.is_public = False
+
     class_.brief = cbg.Description()
     class_.brief.add('ja', 'コライダの抽象基本クラスです')
 
     with class_.add_property(Vector2F, 'Position') as prop_:
         prop_.has_getter = True
         prop_.has_setter = True
+        prop_.is_public = True
         prop_.brief = cbg.Description()
         prop_.brief.add('ja', 'コライダの位置情報を取得または設定します。')
 
     with class_.add_property(float, 'Rotation') as prop_:
         prop_.has_getter = True
         prop_.has_setter = True
+        prop_.is_public = True
         prop_.brief = cbg.Description()
         prop_.brief.add('ja', 'コライダの回転情報を取得または設定します。')
         
     with class_.add_func('GetIsCollidedWith') as func_:
         func_.add_arg(Collider, 'collider')
         func_.return_value.type_ = bool
+        func_.is_public = True
         func_.brief = cbg.Description()
-        func.brief.add('ja', '指定したコライダとの衝突判定を行います。')
+        func_.brief.add('ja', '指定したコライダとの衝突判定を行います。')
 
 CircleCollider = cbg.Class('Altseed', 'CircleCollider')
 with CircleCollider as class_:
@@ -35,6 +39,7 @@ with CircleCollider as class_:
     class_.base_class = Collider
     class_.brief = cbg.Description()
     class_.brief.add('ja', '円形コライダのクラス')
+
     with class_.add_property(float, 'Radius') as prop_:
         prop_.has_getter = True
         prop_.has_setter = True
