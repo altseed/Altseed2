d4fbff26de8a1a8ae8cee05a8b42ba5e5b03c145
wraikny <writtenknown@gmail.com>
2020-04-17 23:46:18 +0900

    impl CopyTexture

--- core/test/Graphics.cpp
+++ core/test/Graphics.cpp
@@ -21,6 +21,7 @@
 #include "Graphics/Renderer/RenderedText.h"
 #include "Graphics/Renderer/Renderer.h"
 #include "Graphics/Shader.h"
+#include "Graphics/Material.h"
 #include "Graphics/ShaderCompiler/ShaderCompiler.h"
 #include "Logger/Log.h"
 #include "Math/Matrix44F.h"
@@ -652,6 +653,74 @@ TEST(Graphics, CullingTooManySprite) {
         }
 
         Altseed::Renderer::GetInstance()->Render();
+
+        EXPECT_TRUE(instance->EndFrame());
+    }
+
+    Altseed::Core::Terminate();
+}
+
+const char* PostEffectCode = R"(
+Texture2D mainTex : register(t0);
+SamplerState mainSamp : register(s0);
+cbuffer Consts : register(b1)
+{
+    float4 time;
+};
+struct PS_INPUT
+{
+    float4  Position : SV_POSITION;
+    float4  Color    : COLOR0;
+    float2  UV1 : UV0;
+    float2  UV2 : UV1;
+};
+float4 main(PS_INPUT input) : SV_TARGET 
+{ 
+    if (input.UV1.x > 0.5) {
+        return float4(input.UV1, 1.0, 1.0);
+    }
+
+    float x = frac(input.UV1.x + time.x * 0.5 - floor(input.UV1.y * 10) * 0.1);
+
+    float4 tex = mainTex.Sample(mainSamp, float2(x, input.UV1.y));
+    
+    return float4(tex.xyz, 1.0);
+}
+)";
+
+TEST(Graphics, PostEffect) {
+    EXPECT_TRUE(Altseed::Core::Initialize(u"SpriteTexture", 1280, 720, Altseed::Configuration::Create()));
+
+    auto instance = Altseed::Graphics::GetInstance();
+    auto cmdList = instance->GetCommandList();
+
+    auto t1 = Altseed::Texture2D::Load(u"TestData/IO/AltseedPink.png");
+    EXPECT_TRUE(t1 != nullptr);
+
+    auto s1 = Altseed::RenderedSprite::Create();
+    s1->SetTexture(t1);
+    s1->SetSrc(Altseed::RectF(0, 0, 400, 400));
+
+    auto ps = Altseed::Shader::Create(u"posteffect", Altseed::utf8_to_utf16(PostEffectCode).c_str(), Altseed::ShaderStageType::Pixel);
+    auto material = Altseed::MakeAsdShared<Altseed::Material>();
+    material->SetShader(ps);
+
+    auto buffer = Altseed::RenderTexture::Create(Altseed::Window::GetInstance()->GetSize());
+
+    int count = 0;
+    while (count++ < 180 && instance->DoEvents()) {
+        Altseed::CullingSystem::GetInstance()->UpdateAABB();
+        Altseed::CullingSystem::GetInstance()->Cull(Altseed::RectF(Altseed::Vector2F(), Altseed::Window::GetInstance()->GetSize().To2F()));
+
+        material->SetVector4F(u"time", Altseed::Vector4F(count / 180.0, 0.0, 0.0, 0.0));
+        EXPECT_TRUE(instance->BeginFrame());
+
+        Altseed::Renderer::GetInstance()->DrawSprite(s1);
+        Altseed::Renderer::GetInstance()->Render();
+        
+        cmdList->CopyTexture(cmdList->GetScreenTexture(), buffer);
+        material->SetTexture(u"mainTex", buffer);
+        cmdList->RenderToRenderTarget(material);
 
         EXPECT_TRUE(instance->EndFrame());
     }
