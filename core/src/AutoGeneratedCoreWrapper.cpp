ec2d7c2b964ac4f0876733e7b316819e259e1296
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-04-27 08:33:39 +0900

    update wrapper

--- core/src/AutoGeneratedCoreWrapper.cpp
+++ core/src/AutoGeneratedCoreWrapper.cpp
@@ -4087,29 +4087,3 @@ CBGEXPORT void CBGSTDCALL cbg_PolygonCollider_Release(void* cbg_self) {
 
 }
 
-#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-	bool res = true;
-	switch (fdwReason) {
-	case DLL_PROCESS_ATTACH:
-		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |COINIT_DISABLE_OLE1DDE);
-		break;
-	case DLL_PROCESS_DETACH:
-		CoUninitialize();
-		break;
-	case DLL_THREAD_ATTACH:
-		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |COINIT_DISABLE_OLE1DDE);
-		break;
-	case DLL_THREAD_DETACH:
-		CoUninitialize();
-		break;
-	default:
-		break;
-	}
-	return res;
-}
-
-#endif
-
