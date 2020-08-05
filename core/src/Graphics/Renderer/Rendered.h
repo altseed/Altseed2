49a5f2bbad7ba2002a4ba8c057ead5cfa5df1428
GON <38759162+gon6109@users.noreply.github.com>
2020-08-06 00:54:33 +0900

    update: culling system
    * update: renderedCamera
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * update: culling system
    
    * fix: lint

--- core/src/Graphics/Renderer/Rendered.h
+++ core/src/Graphics/Renderer/Rendered.h
@@ -12,16 +12,12 @@ class Rendered : public BaseObject {
 protected:
     Matrix44F transform_;
     std::shared_ptr<CullingSystem> cullingSystem_;
-    bool isDrawn_;
 
 public:
     Rendered();
     virtual ~Rendered();
     const Matrix44F& GetTransform() const;
     void SetTransform(const Matrix44F& transform);
-
-    bool SetIsDrawn(bool isDrawn) { return isDrawn_ = isDrawn; }
-    bool GetIsDrawn() { return isDrawn_; }
 
     virtual b2AABB GetAABB() { return b2AABB(); }
 };
