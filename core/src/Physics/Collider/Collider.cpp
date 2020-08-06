9b49a915a36016bd2369b36230d730ba59daed95
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-08-06 15:11:46 +0900

    fix Setter func

--- core/src/Physics/Collider/Collider.cpp
+++ core/src/Physics/Collider/Collider.cpp
@@ -13,20 +13,31 @@ Vector2F Collider::GetPosition() const { return position_; }
 void Collider::SetPosition(Vector2F position) {
     position_ = position;
     transform_.p = b2Vec2(position.X, position.Y);
-    transformMatrix_ = transformMatrix_.SetTranslation(position.X, position.Y, 0);
+    Matrix44F t, r;
+    t.SetTranslation(position.X, position.Y, 1);
+    r.SetRotationZ(rotation_);
+    transformMatrix_ = t * r;
 }
 
 double Collider::GetRotation() const { return rotation_; }
 void Collider::SetRotation(double rotation) {
     rotation_ = rotation;
     transform_.q = b2Rot(rotation);
-    transformMatrix_ = transformMatrix_.SetRotationZ(rotation);
+    Matrix44F t, r;
+    t.SetTranslation(position_.X, position_.Y, 1);
+    r.SetRotationZ(rotation);
+    transformMatrix_ = t * r;
 }
 
 Matrix44F Collider::GetTransform() const { return transformMatrix_; }
 void Collider::SetTransform(const Matrix44F& transform) {
     transform_ = Box2DHelper::ToBox2D_Mat(transform);
     transformMatrix_ = transform;
+    double r;
+    Vector2F p, s;
+    Matrix44F::CalcFromTransform2D(transform, r, p, s);
+    position_ = p;
+    rotation_ = r;
 }
 
 bool Collider::GetIsCollidedWith(std::shared_ptr<Collider> collider) { return GetIsCollidedWith_(collider); }
