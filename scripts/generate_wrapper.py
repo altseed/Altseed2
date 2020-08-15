d561a874712706b74c4508eb05ad1811a04e95e7
wisteria <wisnkcs@gmail.com>
2020-08-15 15:18:54 +0900

    update CBG

--- scripts/generate_wrapper.py
+++ scripts/generate_wrapper.py
@@ -12,55 +12,45 @@ if __name__ == '__main__':
     sharedObjectGenerator = cbg.SharedObjectGenerator(define)
 
     sharedObjectGenerator.header = '''
-#include "Core.h"
 #include "BaseObject.h"
-
 #include "Common/Array.h"
-#include "Common/ResourceContainer.h"
 #include "Common/Resource.h"
+#include "Common/ResourceContainer.h"
 #include "Common/Resources.h"
-
-#include "Window/Window.h"
-
+#include "Core.h"
+#include "Graphics/BuiltinShader.h"
+#include "Graphics/CommandList.h"
+#include "Graphics/Font.h"
+#include "Graphics/Graphics.h"
+#include "Graphics/ImageFont.h"
+#include "Graphics/Renderer/RenderedText.h"
+#include "Graphics/Renderer/RenderedCamera.h"
+#include "Graphics/Renderer/RenderedSprite.h"
+#include "Graphics/Renderer/RenderedPolygon.h"
+#include "Graphics/Renderer/Renderer.h"
+#include "Graphics/RenderTexture.h"
+#include "Graphics/ShaderCompiler/ShaderCompiler.h"
+#include "Graphics/Texture2D.h"
+#include "IO/BaseFileReader.h"
+#include "IO/File.h"
+#include "IO/FileRoot.h"
+#include "IO/PackFile.h"
+#include "IO/PackFileReader.h"
+#include "IO/StaticFile.h"
+#include "IO/StreamFile.h"
 #include "Input/ButtonState.h"
 #include "Input/Joystick.h"
 #include "Input/Keyboard.h"
 #include "Input/Mouse.h"
-
-#include "Graphics/Graphics.h"
-#include "Graphics/CommandList.h"
-#include "Graphics/Texture2D.h"
-#include "Graphics/RenderTexture.h"
-#include "Graphics/Font.h"
-#include "Graphics/ImageFont.h"
-#include "Graphics/Renderer/Renderer.h"
-#include "Graphics/Renderer/RenderedSprite.h"
-#include "Graphics/Renderer/RenderedText.h"
-#include "Graphics/Renderer/RenderedPolygon.h"
-#include "Graphics/Renderer/RenderedCamera.h"
-#include "Graphics/BuiltinShader.h"
-#include "Graphics/ShaderCompiler/ShaderCompiler.h"
-
-#include "Tool/Tool.h"
-
-#include "IO/File.h"
-#include "IO/PackFile.h"
-#include "IO/StaticFile.h"
-#include "IO/StreamFile.h"
-#include "IO/FileRoot.h"
-#include "IO/BaseFileReader.h"
-#include "IO/PackFileReader.h"
-
+#include "Logger/Log.h"
 #include "Physics/Collider/Collider.h"
 #include "Physics/Collider/CircleCollider.h"
-#include "Physics/Collider/RectangleCollider.h"
 #include "Physics/Collider/PolygonCollider.h"
-
+#include "Physics/Collider/RectangleCollider.h"
 #include "Sound/Sound.h"
 #include "Sound/SoundMixer.h"
-
-#include "Logger/Log.h"
-
+#include "Tool/Tool.h"
+#include "Window/Window.h"
     '''
 
     sharedObjectGenerator.func_name_create_and_add_shared_ptr = 'Altseed2::CreateAndAddSharedPtr'
