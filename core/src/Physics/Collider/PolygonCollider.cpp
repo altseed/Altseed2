9784414b24a43e52b56d14d18a34f44817745c2a
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-10-04 01:03:48 +0900

    fix ib

--- core/src/Physics/Collider/PolygonCollider.cpp
+++ core/src/Physics/Collider/PolygonCollider.cpp
@@ -1,5 +1,7 @@
 #include "PolygonCollider.h"
 
+#include "Box2DHelper.h"
+
 namespace Altseed2 {
 
 PolygonCollider::PolygonCollider() {
@@ -12,22 +14,15 @@ PolygonCollider::PolygonCollider() {
 std::shared_ptr<PolygonCollider> PolygonCollider::Create() { return MakeAsdShared<PolygonCollider>(); }
 
 std::shared_ptr<Int32Array> PolygonCollider::GetBuffers() const { return buffers_; }
-void PolygonCollider::SetBuffers(const std::shared_ptr<Int32Array> buffers) { buffers_ = buffers; }
+void PolygonCollider::SetBuffers(const std::shared_ptr<Int32Array> buffers) {
+    buffers_ = buffers;
+    UpdateTriangles();
+}
 
 std::shared_ptr<Vector2FArray> PolygonCollider::GetVertexes() const { return vertexes_; }
 void PolygonCollider::SetVertexes(std::shared_ptr<Vector2FArray> vertexes) {
     vertexes_ = vertexes;
-
-    int triangleCount = vertexes->GetCount() - 2;
-    triangles_.resize(triangleCount);
-    for (int i = 0; i < triangleCount; ++i) {
-        b2PolygonShape triangle;
-        triangle.m_count = 3;
-        triangle.m_vertices[0] = b2Vec2(vertexes->GetVector()[0].X, vertexes->GetVector()[0].Y);
-        triangle.m_vertices[1] = b2Vec2(vertexes->GetVector()[i + 1].X, vertexes->GetVector()[i + 1].Y);
-        triangle.m_vertices[2] = b2Vec2(vertexes->GetVector()[i + 2].X, vertexes->GetVector()[i + 2].Y);
-        triangles_[i] = triangle;
-    }
+    UpdateTriangles();
 }
 
 bool PolygonCollider::GetIsCollidedWith_(std::shared_ptr<Collider> collider) {
@@ -68,10 +63,40 @@ void PolygonCollider::SetDefaultIndexBuffer() {
     }
     auto length = vs.size() - 2;
     buffers_->GetVector().resize(length * 3);
+    triangles_.resize(length);
+
     for (int i = 0; i < length; i++) {
         buffers_->GetVector()[i * 3] = 0;
         buffers_->GetVector()[i * 3 + 1] = i + 1;
         buffers_->GetVector()[i * 3 + 2] = i + 2;
+
+        b2PolygonShape triangle;
+        triangle.m_count = 3;
+        triangle.m_vertices[0] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[0]);
+        triangle.m_vertices[1] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[i + 1]);
+        triangle.m_vertices[2] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[i + 2]);
+        triangles_[i] = triangle;
+    }
+}
+
+void PolygonCollider::UpdateTriangles() {
+    auto buffers = buffers_->GetVector();
+    auto count = buffers.size() / 3;
+    auto vertexesSize = vertexes_->GetVector().size();
+
+    triangles_.resize(count);
+    auto skipCount = 0;
+    for (int i = 0; i < count; i++) {
+        auto ib1 = buffers[i * 3];
+        auto ib2 = buffers[i * 3 + 1];
+        auto ib3 = buffers[i * 3 + 2];
+        if (ib1 >= vertexesSize || ib2 >= vertexesSize || ib3 >= vertexesSize) continue;
+        b2PolygonShape triangle;
+        triangle.m_count = 3;
+        triangle.m_vertices[0] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[ib1]);
+        triangle.m_vertices[1] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[ib2]);
+        triangle.m_vertices[2] = Box2DHelper::ToBox2D_Vec(vertexes_->GetVector()[ib3]);
+        triangles_[i] = triangle;
     }
 }
 
