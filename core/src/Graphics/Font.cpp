c2ae8a328d5f2a77984edd97d577e4cb73a29660
durswd <durswd@gmail.com>
2020-11-10 21:38:05 +0900

    wip

--- core/src/Graphics/Font.cpp
+++ core/src/Graphics/Font.cpp
@@ -4,6 +4,7 @@
 
 #undef INFINITE
 #include <msdfgen/msdfgen.h>
+#include <msdfgen/msdfgen-ext.h>
 
 #include <sstream>
 #include <string>
@@ -21,6 +22,33 @@
 #endif
 
 namespace Altseed2 {
+
+
+using namespace msdfgen;
+
+int testcode() {
+    FreetypeHandle *ft = initializeFreetype();
+    if (ft) {
+        FontHandle *font = loadFont(ft, "C:\\Windows\\Fonts\\arialbd.ttf");
+        if (font) {
+            Shape shape;
+            if (loadGlyph(shape, font, 'A')) {
+                shape.normalize();
+                //                      max. angle
+                edgeColoringSimple(shape, 3.0);
+                //           image width, height
+                Bitmap<float, 3> msdf(32, 32);
+                //                     range, scale, translation
+                generateMSDF(msdf, shape, 4.0, 1.0, Vector2(4.0, 4.0));
+                savePng(msdf, "output.png");
+            }
+            destroyFont(font);
+        }
+        deinitializeFreetype(ft);
+    }
+    return 0;
+}
+
 
 Glyph::Glyph(Vector2I textureSize, int32_t textureIndex, Vector2I position, Vector2I size, Vector2I offset, int32_t glyphWidth)
     : textureSize_(textureSize), textureIndex_(textureIndex), position_(position), size_(size), offset_(offset), glyphWidth_(glyphWidth) {}
